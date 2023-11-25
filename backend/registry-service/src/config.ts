862191187df3c50a8a61150173db2aca34e98118
Hamza SARI <hamza.sari@gmail.com>
2023-11-25 16:03:26 +0100

    Add REGISTERED_SERVICE_TIMEOUT to config

--- backend/registry-service/src/config.ts
+++ backend/registry-service/src/config.ts
@@ -6,5 +6,6 @@ dotenv.config();
 export default {
   serviceName: pkg.name,
   serviceVersion: pkg.version,
-  servicePort: process.env.PORT || 3080
+  servicePort: process.env.PORT || 3080,
+  registeredServiceTimeout: Number(process.env.REGISTERED_SERVICE_TIMEOUT) || 15
 };
